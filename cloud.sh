ae25517aea325cf9fe2e29cae38a23e875d67548
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-08-15 11:19:54 +0200

    windows package stuff
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cloud.sh
+++ cloud.sh
@@ -8,5 +8,8 @@ sleep 60
 cf-remote --version master install --demo --bootstrap ubuntu --hub ubuntu
 cf-remote --version master install --demo --bootstrap ubuntu --clients windows
 
+# cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9652/PACKAGES_HUB_x86_64_linux_ubuntu_22/cfengine-nova-hub_3.23.0a.de4cfaffc~28034.ubuntu22_amd64.deb" --demo --bootstrap ubuntu --hub ubuntu
+# cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9652/PACKAGES_x86_64_mingw/cfengine-nova-3.23.0a-28034-x86_64.msi" --demo --bootstrap ubuntu --clients windows
+
 cfbs build
 cf-remote deploy --hub ubuntu
