22bdd7134fedc3304f4124e70e7202bfd9d2cc81
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-08-16 16:56:10 +0200

    Use custom packages
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cloud.sh
+++ cloud.sh
@@ -5,11 +5,11 @@ cf-remote spawn --platform windows-2016-x64 --name windows --count 1 --role clie
 
 sleep 60
 
-cf-remote --version master install --demo --bootstrap ubuntu --hub ubuntu
-cf-remote --version master install --demo --bootstrap ubuntu --clients windows
+# cf-remote --version master install --demo --bootstrap ubuntu --hub ubuntu
+# cf-remote --version master install --demo --bootstrap ubuntu --clients windows
 
-# cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9652/PACKAGES_HUB_x86_64_linux_ubuntu_22/cfengine-nova-hub_3.23.0a.de4cfaffc~28034.ubuntu22_amd64.deb" --demo --bootstrap ubuntu --hub ubuntu
-# cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9652/PACKAGES_x86_64_mingw/cfengine-nova-3.23.0a-28034-x86_64.msi" --demo --bootstrap ubuntu --clients windows
+cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9670/PACKAGES_HUB_x86_64_linux_ubuntu_20/cfengine-nova-hub_3.23.0a.186345f23~28238.ubuntu20_amd64.deb" --demo --bootstrap ubuntu --hub ubuntu
+cf-remote install --package "http://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-9670/PACKAGES_i386_mingw/cfengine-nova-3.23.0a-28238-i686.msi" --demo --bootstrap ubuntu --clients windows
 
 cfbs build
 cf-remote deploy --hub ubuntu
