f10d2ed91d83d94fa7812cc87b40523ca5bc7b8e
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-11-28 16:14:53 +0100

    Debugging remote copy
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cloud.sh
+++ cloud.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cf-remote spawn --platform ubuntu-24 --name aws-hub --count 1 --role hub
-cf-remote spawn --platform ubuntu-24 --name aws-cli --count 1 --role client
+cf-remote spawn --platform ubuntu-24 --name aws-ubu --count 1 --role client
+cf-remote spawn --platform debian-12 --name aws-deb --count 1 --role client
 
 sleep 30
-
-cf-remote install --bootstrap aws-hub --hub     aws-hub --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-11439/PACKAGES_HUB_x86_64_linux_ubuntu_24/cfengine-nova-hub_3.21.6-1.ubuntu24_amd64.deb"
-cf-remote install --bootstrap aws-hub --clients aws-cli --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-11439/PACKAGES_x86_64_linux_ubuntu_24/cfengine-nova_3.21.6-1.ubuntu24_amd64.deb"
+ 
+cf-remote install --bootstrap aws-hub --hub     aws-hub --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-11441/PACKAGES_HUB_x86_64_linux_ubuntu_24/cfengine-nova-hub_3.24.1-1.ubuntu24_amd64.deb"
+cf-remote install --bootstrap aws-hub --clients aws-ubu --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-11441/PACKAGES_x86_64_linux_ubuntu_24/cfengine-nova_3.24.1-1.ubuntu24_amd64.deb"
+cf-remote install --bootstrap aws-hub --clients aws-deb --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-11441/PACKAGES_x86_64_linux_debian_12/cfengine-nova_3.24.1-1.debian12_amd64.deb"
