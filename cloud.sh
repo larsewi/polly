3c7d68ae4fa610287f7c12c24068859316618762
Lars Erik Wik <lars.erik.wik@northern.tech>
2026-03-23 18:40:00 +0100

    uio
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cloud.sh
+++ cloud.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-cf-remote spawn --platform ubuntu-24 --name aws-hub --count 1 --role hub
-cf-remote spawn --platform debian-12 --name aws-deb --count 1 --role client
+cf-remote spawn --platform debian-12 --name aws-hub --count 1 --role hub
+cf-remote spawn --platform debian-12 --name aws-cli --count 1 --role client
 
 sleep 30
 
-for host in "aws-hub aws-cli"; do
-    cf-remote run  --hosts "$host" 'echo "set -o vi" >> .bashrc'
-    cf-remote run  --hosts "$host" 'echo "export VISUAL=vim" >> .bashrc'
-    cf-remote run  --hosts "$host" 'echo "export EDITOR=vim" >> .bashrc'
-    cf-remote sudo --hosts "$host" 'echo "set -o vi" >> .bashrc'
-    cf-remote sudo --hosts "$host" 'echo "export VISUAL=vim" >> .bashrc'
-    cf-remote sudo --hosts "$host" 'echo "export EDITOR=vim" >> .bashrc'
-
-exit 0
-
-cf-remote --version master install --bootstrap aws-hub --hub     aws-hub
-cf-remote --version master install --bootstrap aws-hub --clients aws-deb
+cf-remote install --demo --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-12963/PACKAGES_HUB_x86_64_linux_debian_12/cfengine-nova-hub_3.21.8-1.debian12_amd64.deb" --bootstrap aws-hub --hub     aws-hub
+cf-remote install --package "https://buildcache.cfengine.com/packages/testing-pr/jenkins-pr-pipeline-12963/PACKAGES_x86_64_linux_debian_12/cfengine-nova_3.21.8-1.debian12_amd64.deb" --bootstrap aws-hub --clients aws-cli
 
